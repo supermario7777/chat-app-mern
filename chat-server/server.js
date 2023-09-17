3361beb951c1fb8768fa45d5ab9843bec9c87253
Admin <syubebayevm@gmail.com>
2023-09-17 23:30:42 +0500

    test

--- chat-server/server.js
+++ chat-server/server.js
@@ -28,4 +28,51 @@ app.use( errorHandler );
 
 const PORT = process.env.PORT || 3001
 
-app.listen( PORT, console.log( `Server is running on Port ${PORT}`.yellow.bold ) )
+const server = app.listen( PORT, console.log( `Server is running on Port ${PORT}`.yellow.bold ) )
+
+const io = require( "socket.io" )( server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:3000",
+    }
+} );
+
+io.on( "error", ( err ) =>
+{
+    console.error( "Socket.IO Error:", err );
+} );
+
+
+io.on( "connection", (socket) =>
+{
+    console.log( "connected to socket.io" );
+
+    socket.on( "setup", ( userData ) =>
+    {
+        socket.join( userData._id );
+        socket.emit( "connected" )
+    } );
+
+    socket.on( "join chat", ( room ) =>
+    {
+        socket.join( room );
+        console.log( "User joined to the room: " + room );
+    } );
+
+    socket.on( "typing", ( room ) => socket.in( room ).emit( "typing" ) );
+    socket.on( "stop typing", ( room ) => socket.in( room ).emit( "stop typing" ) );
+    socket.on( 'new message', (newMessageReceived) =>
+    {
+        var chat = newMessageReceived.chat;
+
+        if ( !chat.users ) return console.log( "chat.users not defined" );
+
+        chat.users.forEach(user => {
+            if ( user._id == newMessageReceived.sender._id ) {
+                return; 
+            }
+
+            socket.in( user._id ).emit( "message received", newMessageReceived );
+        });
+    })
+})
